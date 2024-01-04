fad419da95f10bb73c06eee31db854a08644f998
Angel <gelvictorio08@gmail.com>
2024-01-04 11:04:10 +0000

    Edited esri-map.js

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [-122.31, 47.60],
+            center: [120.9842, 14.5995],
             container: 'map',
             map: map,
             zoom: 12

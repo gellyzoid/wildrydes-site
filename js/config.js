e663a102cadd82c2281d33f56817ba8668628404
gellyzoid <39223824+gellyzoid@users.noreply.github.com>
2024-02-13 21:56:16 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_1cYwrxmdq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'rj0k6edoqj8k0bbfj0hule0p2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://38tbbzmusd.execute-api.ap-northeast-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
